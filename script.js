10bebc98049aff759d86c780922987828adab043
Marina Domrachova <apaycheva@gmail.com>
2020-07-02 20:07:27 +0300

    Box

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-script.js
+let box = document.querySelector(".box");
+let moveBy = 200;
+let moved = parseInt(box.style.marginLeft) || 0;
+
+do {
+  box.style.marginLeft = ++moved + "px";
+} while (moved <= moveBy -1); 
