d4315b21869e5eea8742fc5da914c78095584aff
Marina Domrachova <apaycheva@gmail.com>
2020-07-02 20:56:57 +0300

    For Break

--- script.js
+++ script.js
@@ -2,6 +2,24 @@ let box = document.querySelector(".box");
 let moveBy = 200;
 let moved = parseInt(box.style.marginLeft) || 0;
 
-do {
-  box.style.marginLeft = ++moved + "px";
-} while (moved < moveBy); 
+//do {
+  //box.style.marginLeft = ++moved + "px";
+//} while (moved < moveBy); 
+
+//while (moved < moveBy) {
+  //box.style.marginLeft = ++moved + "px";  
+//}
+
+//for (let number = 1, isUserHappy = true; isUserHappy; number++) {
+  //isUserHappy = confirm ("Are you happy that number is " + number + " ?")
+//}
+
+//for (var i = 1; i < 10; i++) {
+  //if (!(i % 2)) break;
+  //console.log(i);
+//}
+
+for (var i = 1; i < 10; i++) {
+  if (i % 2) continue;
+  console.log(i);
+}
